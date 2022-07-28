1bfcd654e22f0fb8889e54ebd26977ea8fdab836
ZhangLixian1023 <80459388+ZhangLixian1023@users.noreply.github.com>
2022-07-28 17:47:07 +0800

    修改上报数据为在家的数据

--- runme.py
+++ runme.py
@@ -2,23 +2,19 @@ import argparse
 from Report import Report
 
 report_data = [
-    ("juzhudi", "中校区"),  # 居住地
-    ("dorm_building", "z2"),  # 宿舍楼
-    ("dorm", "414"),  # 宿舍号
-    ("body_condition", "1"),  # 身体状况 1为正常
-    ("body_condition_detail", ""),  # 身体状况详细 正常无需
-    ("now_status", "1"),  # 当前状态 1为正常在校园内
-    ("now_status_detail", ""),  # 当前状态详细 正常无需
-    ("has_fever", "0"),  # 目前有无发热等疑似症状 0为无
-    ("last_touch_sars", "0"),  # 是否接触过确诊或疑似病例的患者 0为无
-    ("last_touch_sars_date", ""),  # 最近一次接触日期
-    ("last_touch_sars_detail", ""),  # 具体情况
-    ("is_danger", "0"),  # 当前居住地是否为疫情中高风险地区 0为否
-    ("is_goto_danger", "0"),  # 14天内是否有疫情中高风险地区旅居史 0为否
-    ("jinji_lxr", "cyzkrau"),  # 紧急联系人
-    ("jinji_guanxi", "self"),  # 与本人关系
-    ("jiji_mobile", "18701375065"),  # 紧急联系人电话
-    ("other_detail", ""),  # 其他情况说明
+    "juzhudi": "河南省郑州市新郑市",
+    "body_condition": "1",
+    "body_condition_detail": "",
+    "has_fever": "0",
+    "last_touch_sars": "0",
+    "last_touch_sars_date": "",
+    "last_touch_sars_detail": "",
+    "is_danger": "0",
+    "is_goto_danger": "0",
+    "jinji_lxr": "张昆",
+    "jinji_guanxi": "父",
+    "jiji_mobile": "13803714909",
+    "other_detail": ""
 ]
 cross_campus_data = [
     ("return_college[]", "东校区"),  # 往返校区
