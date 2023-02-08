d0529e59afe91a56258ca24181a7f8e1746cea5e
ZhangLixian1023 <80459388+ZhangLixian1023@users.noreply.github.com>
2023-02-08 22:35:25 +0800

    去掉行程卡,去掉跨校出校,只保留在家报备

--- Report.py
+++ Report.py
@@ -1,24 +1,12 @@
 from bs4 import BeautifulSoup
 from ustclogin import Login
-from datetime import timedelta
-from datetime import timezone
-
-SHA_TZ = timezone(  # 北京时间
-    timedelta(hours=8),
-    name="Asia/Shanghai",
-)
 
 
 class Report(object):
-
     def __init__(self, stuid, password):
         self.stuid = stuid
         self.password = password
         self.login = Login(self.stuid, self.password,"https://weixine.ustc.edu.cn/2020/caslogin")
-    def getstate(self):
-        data = self.login.result.text
-        soup = BeautifulSoup(data, "html.parser")
-        return soup.find("p", {"style":"margin: 5px -10px 0;"}).contents[1].contents[0]
 
     def report(self, report_data):
         data = self.login.result.text
@@ -33,56 +21,4 @@ class Report(object):
         print("健康打卡结果："+info)
         if "上报成功" in info:
             return True
-        return False
-
-    def cross_campus(self, cross_campus_data):
-        data = self.login.session.get("https://weixine.ustc.edu.cn/2020").text
-        soup = BeautifulSoup(data, "html.parser")
-        headers = {
-            "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36 Edg/99.0.1150.39"
-        }
-        data = self.login.session.get("https://weixine.ustc.edu.cn/2020/apply/daliy/i?t=3",headers=headers).text
-        data = data.encode("ascii", "ignore").decode("utf-8", "ignore")
-        soup = BeautifulSoup(data, "html.parser")
-        token = soup.find("input", {"name": "_token"})["value"]
-        start_date = soup.find("input", {"id": "start_date"})["value"]
-        end_date = soup.find("input", {"id": "end_date"})["value"]
-        data = cross_campus_data + [
-            ("_token", token),
-            ("start_date", start_date),
-            ("end_date", end_date),
-            ("t", "3"),
-        ]
-        post = self.login.session.post("https://weixine.ustc.edu.cn/2020/apply/daliy/ipost",data=data)
-        if "?t=d" in post.url:
-            print("跨校区报备：成功")
-            return True
-        print("跨校区报备：失败")
-        return False
-
-    def out_school(self, out_school_data):
-        data = self.login.session.get("https://weixine.ustc.edu.cn/2020").text
-        soup = BeautifulSoup(data, "html.parser")
-        headers = {
-            "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36 Edg/99.0.1150.39"
-        }
-        data = self.login.session.get("https://weixine.ustc.edu.cn/2020/apply/daliy/i?t=2",headers=headers).text
-        data = data.encode("ascii", "ignore").decode("utf-8", "ignore")
-        soup = BeautifulSoup(data, "html.parser")
-        token = soup.find("input", {"name": "_token"})["value"]
-        start_date = soup.find("input", {"id": "start_date"})["value"]
-        end_date = soup.find("input", {"id": "end_date"})["value"]
-        data = out_school_data + [
-            ("_token", token),
-            ("start_date", start_date),
-            ("end_date", end_date),
-            ("t", "2"),
-        ]
-        post = self.login.session.post(
-            "https://weixine.ustc.edu.cn/2020/apply/daliy/ipost",
-            data=data)
-        if "?t=d" in post.url:
-            print("出校报备：成功")
-            return True
-        print("出校报备：失败")
         return False
--- runme.py
+++ runme.py
@@ -2,67 +2,25 @@ import argparse
 from Report import Report
 # 要上传的数据
 report_data =[
-    ("jinji_lxr", "lxr"),
-    ("jiji_mobile", "12345"),
+    ("juzhudi","河南省郑州市新郑市"),
+    ("q_0","良好"),
+    ("body_condition_detail",""),
+    ("q_2",""),
+    ("q_3",""),
+    ("jinji_lxr", "zxx"),
     ("jinji_guanxi", "父"),
-    ("juzhudi", "东校区"),
-    ("dorm_building","365"),
-    ("dorm","304"),
-    ("body_condition", "1"),
-    ("body_condition_detail", ""),
-    ("has_fever", "0"),
-    ("last_touch_sars", "0"),
-    ("last_touch_sars_date", ""),
-    ("last_touch_sars_detail", ""),
-    ("is_danger", "0"),
-    ("is_goto_danger", "0"),
+    ("jiji_mobile", "4214"),
     ("other_detail", "")
 ]
-cross_campus_data = [
-    ("return_college[]", "东校区"),  # 往返校区
-    ("return_college[]", "西校区"),  # 往返校区
-    #("return_college[]", "南校区"),  # 往返校区
-    #("return_college[]", "北校区"),  # 往返校区
-    ("return_college[]", "中校区"),  # 往返校区
-    ("reason", "去图书馆"),  # 原因
-]
-out_school_data = [
-    ("return_college[]", "蜀山区"),  # 目的地
-    ("return_college[]", "包河区"),  # 目的地
-    ("return_college[]", "瑶海区"),  # 目的地
-    ("return_college[]", "庐阳区"),  # 目的地
-    ("reason", "去商场"),  # 原因
-]
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="学号密码必填. 其余项选填, 语法是 -a (表示y), -a y 或 -a n, 空缺则采取默认值")
+    parser = argparse.ArgumentParser()
     parser.add_argument("stuid", help="学号", type=str)
     parser.add_argument("password", help="密码", type=str)
     parser.add_argument("jjlxr", help="紧急联系人", type=str)
     parser.add_argument("jjdh", help="紧急电话", type=str)
-    parser.add_argument('-r', help="上报健康信息, 默认值y",nargs='?', default='y',const='y',choices=['y','n'],metavar='y/n')
-    parser.add_argument('-u',help="更新行程卡, 默认值y", nargs='?', default='y',const='y',choices=['y','n'],metavar='y/n')
-    parser.add_argument('-c', help="跨校区, 默认值n", nargs='?', default='n',const='y',choices=['y','n'],metavar='y/n')
-    parser.add_argument('-o',help="出校, 默认值n",nargs='?',  default='n',const='y',choices=['y','n'],metavar='y/n')
     args = parser.parse_args()
     autorepoter = Report(stuid=args.stuid, password=args.password)
-    report_data[0]=("jinji_lxr", args.jjlxr)
-    report_data[1]=("jiji_mobile", args.jjdh)
+    report_data[5]=("jinji_lxr", args.jjlxr)
+    report_data[7]=("jiji_mobile", args.jjdh)
     autorepoter.login.login()
-    if args.r=='y':
-        autorepoter.report(report_data)
-    if args.u=='y':
-        pass
-
-    state=autorepoter.getstate()
-    if args.c=='y':
-        if state != '在校':
-            print(state+", 不做跨校报备")
-            exit(-1)
-        else:
-            autorepoter.cross_campus(cross_campus_data)
-    if args.o=='y':
-        if state != '在校':
-            print(state+", 不做出校报备")
-            exit(-1)
-        else:
-            autorepoter.out_school(out_school_data)
+    autorepoter.report(report_data)
