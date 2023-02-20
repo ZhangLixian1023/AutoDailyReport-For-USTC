8440f3578f558729021258c50b30286f571b0a23
ZhangLixian1023 <80459388+ZhangLixian1023@users.noreply.github.com>
2023-02-21 01:15:48 +0800

    改为在校并增加跨校区自动报备

--- Report.py
+++ Report.py
@@ -22,3 +22,17 @@ class Report(object):
         if "上报成功" in info:
             return True
         return False
+    def cross_campus(self, cross_campus_data):
+        data = self.login.result.text
+        data = data.encode("ascii", "ignore").decode("utf-8", "ignore")
+        soup = BeautifulSoup(data, "html.parser")
+        token = soup.find("input", {"name": "_token"})["value"]
+        data = [("_token", token)] + cross_campus_data
+        url = "https://weixine.ustc.edu.cn/2020/apply/daliy/ipost"
+        r = self.login.session.post(url, data=data)
+        status=BeautifulSoup(r.text, 'html.parser').find(text="你的当前状态：").parent.strong
+        info=status.text
+        print("你的状态："+info)
+        if "可跨校区" in info:
+            return True
+        return False
--- runme.py
+++ runme.py
@@ -1,8 +1,12 @@
 import argparse
+import datetime
+import pytz
 from Report import Report
 # 要上传的数据
 report_data =[
-    ("juzhudi","河南省郑州市新郑市"),
+    ("juzhudi","东校区"),
+    ("dorm_building","365"),
+    ("dorm","304"),
     ("q_0","良好"),
     ("body_condition_detail",""),
     ("q_2",""),
@@ -12,6 +16,16 @@ report_data =[
     ("jiji_mobile", "4214"),
     ("other_detail", "")
 ]
+cross_campus_data=[
+    ("start_date", "2023-02-20 23:49:32"),
+    ("end_date","2023-02-21 23:59:59"),
+    ("return_college[]","东校区"),
+    ("return_college[]","西校区"),
+    ("return_college[]","中校区"),
+    ("reason","校内上课/考试"),
+    ("comment",""), 
+    ("t","3")
+]
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("stuid", help="学号", type=str)
@@ -20,7 +34,15 @@ if __name__ == "__main__":
     parser.add_argument("jjdh", help="紧急电话", type=str)
     args = parser.parse_args()
     autorepoter = Report(stuid=args.stuid, password=args.password)
-    report_data[5]=("jinji_lxr", args.jjlxr)
-    report_data[7]=("jiji_mobile", args.jjdh)
+    report_data[7]=("jinji_lxr", args.jjlxr)
+    report_data[9]=("jiji_mobile", args.jjdh)
+    t=datetime.datetime
+    tz=pytz.timezone('Asia/Shanghai')
+    t_now=t.now(tz)
+    t_start=t_now.replace(minute=t_now.minute-1)
+    t_end=t_start.replace(hour=23,minute=59,second=59)
+    cross_campus_data[0]=("start_date",t_start.strftime("%Y-%m-%d %H:%M:%S"))
+    cross_campus_data[1]=("end_date",t_end.strftime("%Y-%m-%d %H:%M:%S"))
     autorepoter.login.login()
     autorepoter.report(report_data)
+    autorepoter.cross_campus(cross_campus_data)
